512f878453b46930a2fa3453df2145cb148e58a6
RKKP <MOMIRZ@rkkp.dk>
2022-03-29 19:39:55 +0200

    read csv file

---
+++ CsvFileReader/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using (var reader = new StreamReader(@"C:\temp\roles.csv"))
+{
+    //List<string> listA = new List<string>();
+    //List<string> listB = new List<string>();
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        //var values = line.Split(';');
+
+        //listA.Add(values[0]);
+        //listB.Add(values[1]);
+
+        //Console.WriteLine($"INSERT [dbo].[WorkAddresses] ([Location], [Created], [LastUpdated]) VALUES (N\'{line}\', NULL, NULL)");
+        //Console.WriteLine($"INSERT [dbo].[Titles] ([Name], [Created], [LastUpdated]) VALUES (N'{line}', NULL, NULL)");
+        Console.WriteLine($"INSERT [dbo].[ClientRoles] ([Name]) VALUES (N'${line}')");
+    }
+    Console.ReadKey();
+}
