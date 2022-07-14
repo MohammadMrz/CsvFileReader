a669aead7c963958dc984288da71b3ff810ca2ff
Mohammad Mirza <MOMIRZ@rkkp.dk>
2022-07-14 14:24:40 +0200

    Refactor code, more user-friendly messages

--- CsvFileReader/Program.cs
+++ CsvFileReader/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using CsvFileReader;
-
-ReadStyreGruppeMedlemmer.ReadFileAndPrint();
-
+namespace CsvFileReader
+{
+    public class Program
+    {
+        // Main Method
+        public static void Main(string[] args)
+        {
+            ReadStyreGruppeMedlemmer.ReadFileAndPrint();
+        }
+    }
+}
--- CsvFileReader/ReadStyreGruppeMedlemmer.cs
+++ CsvFileReader/ReadStyreGruppeMedlemmer.cs
@@ -5,32 +5,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 namespace CsvFileReader
 {
     public static class ReadStyreGruppeMedlemmer
     {
+        static readonly string rootFolder = @"C:\Temp";
+        static readonly string textFile = @"styregruppemedlemmer.txt";
+        //string filepathProject = "C:\\Repos\\CsvFileReader\\CsvFileReader\\Archive\\";
+
         public static void ReadFileAndPrint()
         {
-            string filename = "styregruppemedlemmer.txt";
+            Console.WriteLine("");
+            Console.WriteLine("Start programmet ved at klikke en vilkårlig knap");
+            Console.ReadKey();
 
-            using (var reader = new StreamReader(@"C:\Repos\CsvFileReader\CsvFileReader\Archive\" + filename))
+            string filepath = $"{rootFolder}\\{textFile}";
+
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine("");
+                Console.WriteLine($"Der er ikke nogen txt fil under: {filepath}");
+                Console.ReadKey();
+            }
+
+            using (var reader = new StreamReader(@"C:\Temp\styregruppemedlemmer.txt"))
             {
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     if (line == null)
                     {
-                        Console.WriteLine("der er en tom linie");
+                        Console.WriteLine("");
+                        Console.WriteLine("Der er en fejl i txt filen");
                         Console.ReadKey();
                     }
                     else
                     {
                         var values = line.Split(',');
+                        string firstname = string.Empty;
+                        string middlename = string.Empty;
+                        string lastname = string.Empty;
 
-                        string firstname = values[0];
-                        string middlename = values[1];
-                        string lastname = values[2];
+                        try
+                        {
+                            firstname = values[0];
+                            middlename = values[1];
+                            lastname = values[2];
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Der er fejl i txt filen");
+                            Console.WriteLine("");
+                            Console.WriteLine(ex.Message);
+                            Console.ReadKey();
+                        }
 
                         //Console.WriteLine($"INSERT [dbo].[WorkAddresses] ([Location], [Created], [LastUpdated]) VALUES (N\'{line}\', NULL, NULL)");
                         //Console.WriteLine($"INSERT [dbo].[Titles] ([Name], [Created], [LastUpdated]) VALUES (N'{line}', NULL, NULL)");
@@ -79,6 +110,8 @@ namespace CsvFileReader
                         lastname = string.Empty;
                     }
                 }
+                Console.WriteLine("");
+                Console.WriteLine("Programmet er afsluttet successfuldt");
                 Console.ReadKey();
             }
         }
